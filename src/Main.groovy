3c9306fcc7c41a88a37514a1f4c8d43459eaf966
Matthew McCullough <matthewm@ambientideas.com>
2011-05-07 15:44:25 +0200

    Added subtract feture

--- src/Main.groovy
+++ src/Main.groovy
@@ -6,4 +6,5 @@ int programmingPoints = 10
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${Square.square(programmingPoints)}"
+println "${programmingPoints} minus 7 bonus points is ${Subtract.subtract(programmingPoints, 7)}"
 println "${programmingPoints} plus 3 bonus points is ${Sum.sum(programmingPoints, 3)}"
