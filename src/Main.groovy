e650f374002f860d56ecad63b6fab7387a607642
Matthew McCullough <matthewm@ambientideas.com>
2011-05-07 19:07:05 +0200

    Fixed a typo in a tested value of division (comment and actual didn't acgree)

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,10 +1,8 @@
-import Square
-
 def name = "Matthew"
 int programmingPoints = 10
 
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${Square.square(programmingPoints)}"
-println "${programmingPoints} minus 7 bonus points is ${Subtract.subtract(programmingPoints, 7)}"
+println "${programmingPoints} divided by 2 bonus points is ${Division.divide(programmingPoints, 2)}"
 println "${programmingPoints} plus 3 bonus points is ${Sum.sum(programmingPoints, 3)}"
