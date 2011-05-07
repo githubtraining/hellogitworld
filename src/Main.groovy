369adba006a1bbf25e957a8622d2b919c994d035
Matthew McCullough <matthewm@ambientideas.com>
2011-05-07 18:37:02 +0200

    Used static imports to simpify the math calls

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,7 +1,10 @@
+import static Square.square
+import static Sum.sum
+
 def name = "Matthew"
 int programmingPoints = 10
 
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
-println "${programmingPoints} squared is ${Square.square(programmingPoints)}"
-println "${programmingPoints} plus 3 bonus points is ${Sum.sum(programmingPoints, 3)}"
+println "${programmingPoints} squared is ${square(programmingPoints)}"
+println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
