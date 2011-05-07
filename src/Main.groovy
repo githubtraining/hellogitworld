333fd9bc94084c3e07e092e2bc9c22bab4476439
Matthew McCullough <matthewm@ambientideas.com>
2011-05-07 15:39:39 +0200

    Added sum function and test call

--- src/Main.groovy
+++ src/Main.groovy
@@ -6,3 +6,4 @@ int programmingPoints = 10
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${Square.square(programmingPoints)}"
+println "${programmingPoints} plus 3 bonus points is ${Sum.sum(programmingPoints, 3)}"
