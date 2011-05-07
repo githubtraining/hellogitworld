818f033c4ae7f26b2b29e904942fa79a5ccaadd0
Matthew McCullough <matthewm@ambientideas.com>
2011-05-07 18:42:32 +0200

    Added call and import for subtraction

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,4 +1,5 @@
 import static Square.square
+import static Subtract.subtract
 import static Sum.sum
 
 def name = "Matthew"
@@ -7,4 +8,5 @@ int programmingPoints = 10
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
+println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
 println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
