57d0bf61e57cdacb309ebd1075257c6bd7e1da81
Matthew McCullough <matthewm@ambientideas.com>
2011-05-07 19:12:44 +0200

    Removing subtraction from the division line of code

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,5 +1,4 @@
 import static Square.square
-import static Subtract.subtract
 import static Division.divide
 import static Sum.sum
 
@@ -9,6 +8,5 @@ int programmingPoints = 10
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
-println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
 println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
 println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
