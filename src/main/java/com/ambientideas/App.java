d2280d000c84f1e595e4dec435ae6c1e6c245367
Matthew McCullough <matthew@github.com>
2012-07-24 22:07:55 -0700

    Adding maven build script

---
+++ src/main/java/com/ambientideas/App.java
@@ -0,0 +1,17 @@
+package com.ambientideas;
+
+/**
+ * Hello again
+ * Hello world!
+ * Hello
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Comment
+        System.out.println( "Hello World!" );
+    }
+}
+
+//foo
