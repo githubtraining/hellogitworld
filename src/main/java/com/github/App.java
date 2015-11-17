5b3be013bd4e31fcd739753421dc6e3a018b134d
SaarYogev <saaryogev@gmail.com>
2015-11-17 16:39:57 +0200

    Changed greeting phrase
    Changed greeting phrase, might cause extra fun for the user.

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello World!" );
+        System.out.println( "Hi User!" );
     }
 }
 
