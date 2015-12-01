ea6d48ef9940741f722a10dfc009df7167328d65
GuyRinat <guyr09@gmail.com>
2015-12-01 15:57:24 +0200

    multiplying numbers
    Literally multiplying the numbers

---
+++ src/main/java/com/github/dd.java
@@ -0,0 +1,13 @@
+package main.java.com.github;
+
+public class dd {
+
+	public static void main(String[] args) {
+		int mult = 1;
+		for(int i=0;i<= 10;i++){
+			mult *= (int)(1+Math.random()*10);
+		}
+		System.out.println(mult+"dd");
+	}
+
+}
