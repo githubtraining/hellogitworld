923577106856a33394e69e4fd221e225deb2cb9d
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2019-10-01 19:46:22 +0700

    Add power to expression

--- src/Main.groovy
+++ src/Main.groovy
@@ -2,6 +2,7 @@ import static Square.square
 import static Division.divide
 import static Subtract.subtract
 import static Sum.sum
+import static Power.power
 
 def name = "Matthew"
 int programmingPoints = 10
@@ -12,3 +13,4 @@ println "${programmingPoints} squared is ${square(programmingPoints)}"
 println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
 println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
 println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
+println "${programmingPoints} multiplied itself 3 times is ${power(programmingPoints, 3)}"
---
+++ src/Power.groovy
@@ -0,0 +1,5 @@
+static int power(int val1, val2) {
+    int x = 1;
+    for(int i = 0;i < val2; i++) x *= val1;
+    return x;
+}
